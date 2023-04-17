3f97fc323c2abc62f39ea0aee6e3a5f7940e11f0
vnhung0064 <106472760+vnhung0064@users.noreply.github.com>
2023-04-17 11:29:54 +0700

    Initial Commit

---
+++ NetFlick/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetFlick
+//
+//  Created by Hung Vu on 17/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
